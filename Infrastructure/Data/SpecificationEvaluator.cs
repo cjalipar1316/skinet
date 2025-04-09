ca57f6f9da17fa87ac683d9d84b0ba3119b7ae95
cjalipar1316 <crisjohnalipar@gmail.com>
2025-04-09 18:41:25 +0800

    End of section 5

--- Infrastructure/Data/SpecificationEvaluator.cs
+++ Infrastructure/Data/SpecificationEvaluator.cs
@@ -27,6 +27,11 @@ public class SpecificationEvaluator<T> where T:BaseEntity
             query = query.Distinct();
         }
 
+        if(spec.isPagingEnabled)
+        {
+            query = query.Skip(spec.Skip).Take(spec.Take);
+        }
+
         return query;
     }
     public static IQueryable<TResult> GetQuery<TSpec, TResult>(IQueryable<T> query, ISpecification<T, TResult> spec)
@@ -54,6 +59,11 @@ public class SpecificationEvaluator<T> where T:BaseEntity
 
         if(spec.IsDistinct){
             selectQuery = selectQuery?.Distinct();
+        }
+
+        if(spec.isPagingEnabled)
+        {
+            selectQuery = selectQuery?.Skip(spec.Skip).Take(spec.Take);
         }
 
         return selectQuery ?? query.Cast<TResult>();
